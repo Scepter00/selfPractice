03d77fd46ced7c73dfb617685bc56d8f95ef2846
scepter <chukwunenyeebuka@gmail.com>
2023-12-14 13:30:55 +0100

    fifth update

--- src/chapterTwo/Multiples.java
+++ src/chapterTwo/Multiples.java
@@ -1,5 +1,7 @@
 package chapterTwo;
 
+import java.util.Scanner;
+
 public class Multiples {
 
     /*(Multiples) Write an application that reads two integers, determines whether the first
@@ -7,6 +9,19 @@ public class Multiples {
  remainder operator.]*/
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter first integer: ");
+        int num = input.nextInt() * 3;
+        System.out.print("Enter second integer: ");
+        int num2 = input.nextInt() * 2;
+        int answer = num % num2;
+
+        if (answer == 0) {
+            System.out.println("The triple of " + num + " is a multiple of the double of " + num2 + ": " + answer + ".");
+            }
+        else {
+            System.out.println("");
+        }
     }
 }
