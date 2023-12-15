2743c44920cf6727b40045cac9fe992eceb4b91e
scepter <chukwunenyeebuka@gmail.com>
2023-12-15 18:29:20 +0100

    update

--- src/chapterThree/ClassPetrolPurchaseTest.java
+++ src/chapterThree/ClassPetrolPurchaseTest.java
@@ -1,7 +1,17 @@
 package chapterThree;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClassPetrolPurchaseTest {
+    ClassPetrolPurchase classPetrolPurchase;
+    @BeforeEach
+    void startAllTestWIth() {
+        classPetrolPurchase = new ClassPetrolPurchase("Total","Diesel", 55, 10.5, 70);
+    }
 
+    @Test
+    void
 }
