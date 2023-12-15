50d6844b734d5a131bfeef2273af5345821258cb
scepter <chukwunenyeebuka@gmail.com>
2023-12-15 18:12:05 +0100

    update

---
+++ src/chapterThree/ClassPetrolPurchase.java
@@ -0,0 +1,63 @@
+package chapterThree;
+
+public class ClassPetrolPurchase {
+
+        private String station;
+        private String typeOfPetrol;
+        private int petrolQuantity;
+        private double literOfPetrol;
+        private double petrolDiscount;
+
+        public ClassPetrolPurchase(String station, String typeOfPetrol, int petrolQuantity, double literOfPetrol, double petrolDiscount) {
+            this.station = station;
+            this.typeOfPetrol = typeOfPetrol;
+            this.petrolQuantity = petrolQuantity;
+            this.literOfPetrol = literOfPetrol;
+            this.petrolDiscount = petrolDiscount;
+        }
+
+        public void setStation(String station) {
+            this.station = station;
+        }
+
+        public String getStation() {
+            return station;
+        }
+
+        public void setTypeOfPetrol(String typeOfPetrol) {
+            this.typeOfPetrol = typeOfPetrol;
+        }
+
+        public String getTypeOfPetrol() {
+            return typeOfPetrol;
+        }
+
+        public void setPetrolQuantity(int petrolQuantity) {
+            this.petrolQuantity = petrolQuantity;
+        }
+
+        public int getPetrolQuantity() {
+            return petrolQuantity;
+        }
+
+        public void setLiterOfPetrol(double literOfPetrol) {
+            this.literOfPetrol = literOfPetrol;
+        }
+
+        public double getLiterOfPetrol() {
+            return literOfPetrol;
+        }
+
+        public void setPetrolDiscount(double petrolDiscount) {
+            this.petrolDiscount = petrolDiscount;
+        }
+
+        public double getPetrolDiscount() {
+            return petrolDiscount;
+        }
+
+        public double getPurchaseAmount() {
+            double purchaseAmount = 0;
+            return purchaseAmount * literOfPetrol;
+        }
+}
