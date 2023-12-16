9b1f8d4412d26f19cd0e6a97923710518bde2449
scepter <chukwunenyeebuka@gmail.com>
2023-12-16 11:57:23 +0100

    update

--- src/chapterThree/CarClassTest.java
+++ src/chapterThree/CarClassTest.java
@@ -4,8 +4,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CarClassTest {
     CarClass carClass;
+    CarClass carClass2;
     void startAllTestWith() {
         carClass = new CarClass("Mercedes Benz-AMG GLE 63 S Coupe", "2023", 180_000_000);
+        carClass = new CarClass("Ford Mustang Gt", "2023", 18_900_000);
     }
 
 }
