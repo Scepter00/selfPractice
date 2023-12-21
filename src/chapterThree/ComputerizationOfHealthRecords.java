34ed6af27c37e6abba4ba4620e1968c1a4cb3231
scepter <chukwunenyeebuka@gmail.com>
2023-12-22 00:27:51 +0100

    update

--- src/chapterThree/ComputerizationOfHealthRecords.java
+++ src/chapterThree/ComputerizationOfHealthRecords.java
@@ -20,5 +20,66 @@ son’s first name, last name, gender, date of birth, height and weight—then c
  person’s age in years, BMI, maximum heart rate and target-heart-rate range. It should also display
  the BMI values chart from Exercise 2.33.*/
 
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private int dayOfBirth;
+    private int monthOfBirth;
+    private int yearOfBirth;
+    private double height;
+    private double weight;
+    public ComputerizationOfHealthRecords(String firstName, String lastName, String gender,
+                              int dayOfBirth, int monthOfBirth, int yearOfBirth, double height, double weight) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.dayOfBirth = dayOfBirth;
+        this.monthOfBirth = monthOfBirth;
+        this.yearOfBirth = yearOfBirth;
+        this.height = height;
+        this.weight = weight;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getLastName() { return lastName; }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getGender() { return gender; }
+
+    public void setDayOfBirth(int dayOfBirth) {
+        this.dayOfBirth = dayOfBirth;
+    }
+    public int getDayOfBirth() { return dayOfBirth; }
+
+    public void setMonthOfBirth(int monthOfBirth) {
+        this.monthOfBirth = monthOfBirth;
+    }
+    public int getMonthOfBirth() { return monthOfBirth; }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+    public int getYearOfBirth() { return yearOfBirth; }
+
+    public void setHeight(double height) {
+        this.height = height; }
+    public double getHeight() { return height; }
+
+    public void setWeight(double weight) { this.weight = weight; }
+    public double getWeight() { return weight; }
+
+    public int getAge() {
+        return 2022 - yearOfBirth;
+    }
 }
