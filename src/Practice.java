6009fc195d2a3c324ac4915faacd659c65c19aad
scepter <chukwunenyeebuka@gmail.com>
2023-12-22 14:16:24 +0100

    update

--- src/Practice.java
+++ src/Practice.java
@@ -1,30 +1,3 @@
 public class Practice {
 
-    /* (Gas Mileage) Drivers are concerned with the mileage their automobiles get. One driver has
- kept track of several trips by recording the miles driven and gallons used for each tankful. Develop
- a Java application that will input the miles driven and gallons used (both as integers) for each trip.
- The program should calculate and display the miles per gallon obtained for each trip and print the
- combined miles per gallon obtained for all trips up to this point. All averaging calculations should
- produce floating-point results. Use class Scanner and sentinel-controlled iteration to obtain the data
- from the user.*/
-
-    private int totalMiles;
-    private int totalGallons;
-
-    public void recordTrip(int miles, int gallons) {
-        totalMiles += miles;
-        totalGallons += gallons;
-    }
-    public double calculateMilesPerGallon() {
-        if (totalGallons == 0) {
-            return 0.0; // To avoid division by zero
-        }
-        return (double) totalMiles / totalGallons;
-    }
-    public int getTotalMiles() {
-        return totalMiles;
-    }
-    public int getTotalGallons() {
-        return totalGallons;
-    }
 }
--- src/chapterFour/GasMileageTest.java
+++ src/chapterFour/GasMileageTest.java
@@ -1,5 +1,7 @@
+package chapterFour;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class PracticeTest {
+class GasMileageTest {
 
 }
