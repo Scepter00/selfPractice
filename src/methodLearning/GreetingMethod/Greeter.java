fbff4e4481dc31df27046f7b1e36d524338cac63
scepter <chukwunenyeebuka@gmail.com>
2024-06-01 16:39:39 +0100

    OOP

--- src/methodLearning/GreetingMethod/Greeter.java
+++ src/methodLearning/GreetingMethod/Greeter.java
@@ -4,9 +4,11 @@ public class Greeter {
     private void greet(Greeting greeting) {
         greeting.perform();
     }
+
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
         GreetingsEarthlingsGreeting greetingsEarthlingsGreeting = new GreetingsEarthlingsGreeting();
         greeter.greet(greetingsEarthlingsGreeting);
     }
+
 }
